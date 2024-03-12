fbf459327a56868102aa1144870a1df0f77ed507
Sebastiano Minelli <sebastiano.minelli@mail.polimi.it>
2024-03-12 14:08:38 +0000

    Implemented input for domain dimension

--- main.cpp
+++ main.cpp
@@ -1,32 +1,41 @@
 #include<iostream>
 #include<functional>
 #include<string>
-#include<Eigen>
+#include<vector>
+// #include<Eigen>
 
 int main()
 {
-    using double domain_type; //type of the domain
-    using double codomain_type; //type of the codomain
+    typedef double domain_type; //type of the domain
+    typedef double codomain_type; //type of the codomain
 
-    unknowns = std::vector<domain_type>;
+    std::vector<domain_type> vectors;
 
+    unsigned int n = 0; // dimension of the domain
     double min = 0.0; // minimum
-    double toll_res; // tolerance for the residual
-    double toll_var; // tolerance for the variation
-    double toll_grad; // tolerance for the gradient
-    double max_it; // maximum number of iterations
+    double toll_res = 0.0; // tolerance for the residual
+    double toll_var = 0.0; // tolerance for the variation
+    double toll_grad = 0.0; // tolerance for the gradient
+    double max_it = 0.0; // maximum number of iterations
     std::string min_method; // method to use to compute the minimum
     std::string step_method; // method to use to compute the step coefficient
 
-    bool compute_numeric_grad; // choice to compute gradient numerically instead of providing it directly
+    bool compute_numeric_grad = false; // choice to compute gradient numerically instead of providing it directly
     std::function<codomain_type(std::vector<domain_type>)> fun; // function
     std::function<std::vector<codomain_type>(std::vector<domain_type>)> grad_fun; // function gradient
+
+    while(std::cout << "Insert the dimension of the domain (i.e. the number of variables) " << std::endl && !(std::cin >> n))
+    {
+        std::cin.clear();
+        std::cout << "Wrong value provided" << std::endl;
+    }
+
 
     ///// implementing a function to read user parameters ////
 
    
 
-    min = compute_min(fun, grad_fun, compute_numeric_grad, toll_res, toll_var, toll_grad, max_it);
+    // min = compute_min(fun, grad_fun, compute_numeric_grad, toll_res, toll_var, toll_grad, max_it);
 
     std::cout << "The minimum is: " << min << std::endl;
 
